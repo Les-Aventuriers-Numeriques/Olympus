ca717910e6e191cb5d5881b519e6a082a801ea02
guitouzid <guitouzid@wanadoo.fr>
2013-09-11 13:41:03 +0200

    Modifications sur la page d'accueil 	modified:   docs/Gestion du Projet.xlsx 	modified:   src/Easy/ArticleBundle/Controller/ArticleController.php 	modified:   src/Easy/ArticleBundle/Resources/config/routing.yml 	deleted:    src/Easy/ArticleBundle/Resources/views/Article/index.html.twig 	new file:   src/Easy/ArticleBundle/Resources/views/Article/listAdmin.html.twig 	modified:   src/Easy/EspaceMembreBundle/Resources/views/layout.html.twig 	modified:   src/Easy/SiteBundle/Resources/views/layout.html.twig

--- src/Easy/ArticleBundle/Controller/ArticleController.php
+++ src/Easy/ArticleBundle/Controller/ArticleController.php
@@ -6,10 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleController extends Controller
 {
-    public function indexAction($name)
+    public function listAdminAction()
     {
+        $articles = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:Article')->findAll();
+        
+        return $this->render('EasyArticleBundle:Article:listAdmin.html.twig', array('articles' => $articles));
+    }
+    
+    public function deleteAdminAction($id)
+    {
+        // Récupération de l'article à supprimer
+        $article = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:Article')->findOneById($id);
+        
+        // Suppression
+        $this->getDoctrine()->getManager()->remove($article);
+        $this->getDoctrine()->getManager()->flush();
+        
+        // Retour sur l'administration des articles
+        return $this->redirect($this->generateUrl('easy_article_admin'));
+    }
+    
+    public function updateAdminAction($id=NULL)
+    {
+        // Récupération de l'article à supprimer
+        $article = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:Article')->findOneById($id);
+        if (!$article) $article = new Article();
         
         
-        return 0;
+        $this->getDoctrine()->getManager()->flush();
+        
+        // Retour sur l'administration des articles
+        return $this->redirect($this->generateUrl('easy_article_admin'));
     }
 }

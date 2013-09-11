6f754f20b9b4d3fc4d7bcf340175bc7650f2b0d5
Epoc <contact@epoc.fr>
2013-09-11 20:59:52 +0200

    - Module squald.xml

--- src/Easy/SquadXmlBundle/Controller/DefaultController.php
+++ src/Easy/SquadXmlBundle/Controller/DefaultController.php
@@ -8,6 +8,13 @@ class DefaultController extends Controller
 {
     public function generateAction()
     {
-        
+        // On interdit les requêtes non-Ajax
+        if (!$this->getRequest()->isXmlHttpRequest()) {
+            throw new HttpException('Accès interdit', 401);
+        }
+
+        $soldiers = $this->getDoctrine()->getManager()->getRepository('UtilisateurBundle:Utilisateur')->findAll();
+
+        $squad_xml = $this->renderView('SquadXmlBundle:Default:template.xml.twig', array('soldiers' => $soldiers));
     }
 }

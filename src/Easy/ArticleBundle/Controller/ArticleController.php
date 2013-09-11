1147d403968b4c3b56cdafee9bcd05f7a72a75b9
guitouzid <guitouzid@wanadoo.fr>
2013-09-11 22:01:48 +0200

    Développement du module Don

--- src/Easy/ArticleBundle/Controller/ArticleController.php
+++ src/Easy/ArticleBundle/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace Easy\ArticleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Easy\ArticleBundle\Entity\Article;
 
 class ArticleController extends Controller
 {
@@ -26,13 +27,43 @@ class ArticleController extends Controller
         return $this->redirect($this->generateUrl('easy_article_admin'));
     }
     
-    public function updateAdminAction($id=NULL)
+    public function showAdminAction($id=NULL)
     {
         // Récupération de l'article à supprimer
-        $article = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:Article')->findOneById($id);
+        if ($id==NULL) 
+        {
+            $article = NULL;
+        }
+        else
+        {
+            $article = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:Article')->findOneById($id);
+        }
+        $categories = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:CategorieArticle')->findAll();
+        
+        // Retour sur l'administration des articles
+        return $this->render('EasyArticleBundle:Article:showAdmin.html.twig', array('article' => $article, 'categories' => $categories));
+    }
+    
+    public function updateAdminAction()
+    {
+        // Récupération de l'article à supprimer
+        $article = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:Article')->findOneById($this->getRequest()->request->get('article'));
         if (!$article) $article = new Article();
         
+        // Enregistrement des données
+        $article->setTitre($this->getRequest()->request->get('titre'));
+        $article->setContenu($this->getRequest()->request->get('contenu'));
         
+        $categorie = $this->getDoctrine()->getManager()->getRepository('EasyArticleBundle:CategorieArticle')->findOneById($this->getRequest()->request->get('categorie'));
+        $article->setCategorieArticle($categorie);
+        
+        $utilisateur = $this->getDoctrine()->getManager()->getRepository('EasyUtilisateurBundle:Utilisateur')->findOneByUsername($this->getRequest()->request->get('utilisateur'));
+        $article->setUtilisateur($utilisateur);
+        
+        $article->setDate(new \DateTime('now'));
+        
+        // On save
+        $this->getDoctrine()->getManager()->persist($article);
         $this->getDoctrine()->getManager()->flush();
         
         // Retour sur l'administration des articles

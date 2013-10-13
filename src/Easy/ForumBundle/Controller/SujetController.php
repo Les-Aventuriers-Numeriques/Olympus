5b8f0741b3ad94f57abac9205d55797a92da4a82
guitouzid <guitouzid@wanadoo.fr>
2013-10-13 10:14:11 +0200

    Commit avant que le travail soit écrasé

--- src/Easy/ForumBundle/Controller/SujetController.php
+++ src/Easy/ForumBundle/Controller/SujetController.php
@@ -6,8 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SujetController extends Controller
 {
-    public function indexAction($name)
+    public function listAction($id)
     {
-        return $this->render('EasyForumBundle:Default:index.html.twig', array('name' => $name));
+        $forum = $this->getDoctrine()->getManager()->getRepository('EasyForumBundle:Sujet')->findOneById($id);
+        
+        $sujets = $this->getDoctrine()->getManager()->getRepository('EasyForumBundle:Sujet')->findByForum($forum);
+        
+        return $this->render('EasyForumBundle:Sujet:list.html.twig', array('sujets' => $sujets));
     }
 }
